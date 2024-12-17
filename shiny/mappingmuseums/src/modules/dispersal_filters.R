83027934e2a47e3bbb16de0931c5b2ff66e557b0
George <george.alexander.wright@gmail.com>
2024-12-17 15:49:42 +0000

    fix end of existences in sequences

--- shiny/mappingmuseums/src/modules/dispersal_filters.R
+++ shiny/mappingmuseums/src/modules/dispersal_filters.R
@@ -756,7 +756,8 @@ filtered_sequence_data <- function(
       collection_status %in% collection_status_filter,
       event_id %in% events_with_filtered_destinations$event_id,
       event_id %in% events_with_filtered_recipients$event_id,
-      event_core_type %in% event_type_filter,
+      # it is necessary to add damaged/destroyed here as the core type is not end of existence
+      event_core_type %in% c(event_type_filter, "damaged/destroyed"),
     )
 
   if (steps_or_first_last == "First and last actors") {
