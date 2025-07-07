d636508cd0b73c36444c1e3ba1e9becf42bb28b8
George <george.alexander.wright@gmail.com>
2025-07-07 10:34:24 +0100

    tooltip texts

---
+++ shiny/mappingmuseums/src/texts.R
@@ -0,0 +1,38 @@
+tooltip_view <- "Controls the overall parameters of your search."
+tooltip_filter <- "Allows you to refine your searches. E.g. you might choose to view museums according to size, and then further select according to governance."
+
+tooltip_single_or_range <- "<p><strong>Single year: </strong>Shows museums open in a specified year.</p>
+<p><strong>Range of years: </strong>Shows museums open in a range of years.</p>
+<p>Use the slider below to choose which year or years.</p>"
+
+tooltip_main_attribute <- "Shows all museums or select a museum attribute"
+tooltip_secondary_attribute <- "For use with the heatmap. Choose a second attribute to visualise."
+
+tooltip_museum_governance <- "<p>Shows museums of a specific governance type.</p>
+<p>Governance is the framework by which museums are directed and controlled. It identifies who can make decisions, who has the authority to act on behalf of the organisation, and who is accountable for how the museum and its people perform.</p>
+<p>See the taxonomies tab for the breakdown of governance types.</p>"
+
+tooltip_museum_size <- "<p>Shows museums of a specific size</p>
+<p>Calculated according to the annual number of visits.</p>
+<p><strong>Small: </strong>0 - 10,000 annual visitors.</p>
+<p><strong>Medium: </strong>10,000 - 50,000 annual visitors</p>
+<p><strong>Large: </strong>50,000 - 1,000,000 annual visitors.</p>
+<p><strong>Huge: </strong>More than 1,000,000 annual visitors.</p>"
+
+tooltip_museum_subject <- "<p>Shows museums that focus on a specific subject.</p>
+<p>Subject matter relates to the overall theme of the museum. See the taxonomies tab for a breakdown of topics.</p>"
+
+tooltip_museum_subject_specific <- "<p>Shows museums that focus on a specific subject. See the taxonomies tab for a breakdown of topics.</p>"
+
+tooltip_museum_country_region <- "<p>Shows museums located in a specific nation or region.</p>"
+
+tooltip_museum_accreditation <- "<p>Shows accredited and / or unaccredited museums. Accreditation is the bench-marking process whereby museums are recognised as reaching nationally agreed standards.</p>"
+
+tooltip_collection_type <- "<p>Shows specific types of objects</p>"
+
+tooltip_collection_status <- "<p><strong>Items from a museum's collection: </strong>Items that originally formed part of a now closed museum's collection</p>
+<p><strong>Items loaned to a museum: </strong>Loaned items that were in the custody of a museum when it closed</p>
+<p><strong>Items for handling: </strong>Items that belonged to a now closed museum, but that were not part of the official collection and could be handled for educational purposes.</p>
+<p><strong>Other items: </strong>Items that belonged to a now closed museum, but that were not museum objects. For example display cases and other furniture.</p>"
+
+tooltip_event_types <- "Choose events types: See taxonomies tab for more detail"
