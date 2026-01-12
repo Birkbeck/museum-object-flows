dabcd5ab93071fd31ecd3c13d9a721e0a62bacba
George <george.alexander.wright@gmail.com>
2026-01-12 16:45:38 +0000

    add other llm decoding options

--- notes-to-taxonomy/src/llm.py
+++ notes-to-taxonomy/src/llm.py
@@ -3,5 +3,12 @@ from abc import ABC, abstractmethod
 
 class LLM(ABC):
     @abstractmethod
-    def get_response(self, task_input: str) -> str:
+    def get_response(
+        self,
+        task_input: str,
+        num_return_sequences: int,
+        max_new_tokens: int,
+        temperature: float,
+        seed: int,
+    ) -> str:
         raise NotImplementedError
