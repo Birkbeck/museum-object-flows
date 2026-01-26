b003e4ba9c18de69aec54968657e8e7280500cf3
George <george.alexander.wright@gmail.com>
2026-01-26 16:12:20 +0000

    fix default settings for hpc

--- notes-to-taxonomy/run_taxonomy_building_experiment.py
+++ notes-to-taxonomy/run_taxonomy_building_experiment.py
@@ -4,10 +4,10 @@ from src.experiments import TaxonomyBuildingExperiment
 
 
 def main():
-    with open("experiment-configs/taxonomy-building-local.json", "r") as f:
+    with open("experiment-configs/taxonomy-building.json", "r") as f:
         config = json.load(f)
     experiment = TaxonomyBuildingExperiment.from_config(
-        config, save_label_definitions=False  # TODO: set to true before running on HPC
+        config, save_label_definitions=True
     )
     results = experiment.run()
     print(results)
